0cd226c8190e74720bd089024783f496f6743cfb
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-04-14 01:57:24 +0700

    feat: enable CORS for localhost:3000 in main bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import { LoggerInterceptor } from './common/interceptor/logger.intercptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalFilters(new GlobalExceptionFilter());
